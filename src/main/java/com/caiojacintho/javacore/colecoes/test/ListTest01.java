f414197ea48f9f72f8ecd78a8331048a1d640b52
CaioJacintho2323 <jacinthocaio@gmail.com>
2024-11-12 22:10:10 -0300

    add

--- src/main/java/com/caiojacintho/javacore/colecoes/test/ListTest01.java
+++ src/main/java/com/caiojacintho/javacore/colecoes/test/ListTest01.java
@@ -1,7 +1,14 @@
 package com.caiojacintho.javacore.colecoes.test;
 
+import com.caiojacintho.javacore.colecoes.dominio.Smartphone;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListTest01 {
     public static void main(String[] args) {
-
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Pedro");
     }
 }
